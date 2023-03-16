4c5fda4d628eb1f200ae060c461a847ba54adfb8
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-03-16 12:44:52 +0530

    Domprojects

---
+++ dom2.js
@@ -0,0 +1,10 @@
+// var iemList = document.querySelector ('#items');
+// console.log(item.parentNode);
+// itemList.parentNode.backgroundColor ='#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+/parentElement 
+var iemList = document.querySelector ('#items');
+console.log(item.parentElement );
+itemList.parentElement.backgroundColor ='#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement );
